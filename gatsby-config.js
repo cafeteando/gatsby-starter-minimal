f5d3c0bb98c8a85e5404a20bd06e2406cc8af464
cafeteando <59399031+cafeteando@users.noreply.github.com>
2020-01-06 11:06:18 +0100

    Create gatsby-config.js

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+/**
+ * Configure your Gatsby site with this file.
+ */
+
+module.exports = {
+  /* Your site config here */
+  siteMetadata: {
+    title: `Gatsby Boilerplate Minimal Starter`,
+    siteUrl: `hhttps://github.com/cafeteando/gatsby-starter-minimal`,
+    description: `Blazing fast modern Minimal Starter for Gatsby`,
+  },
+}
